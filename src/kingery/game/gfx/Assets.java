dd9b9883723e91fc135714d404a4e78d3dd98703
SeriousSirToad <lotsoflamps03@gmail.com>
2018-08-28 21:14:26 -0400

    Assets - Minor change

--- src/kingery/game/gfx/Assets.java
+++ src/kingery/game/gfx/Assets.java
@@ -5,7 +5,9 @@ import java.awt.image.BufferedImage;
 public class Assets {
 	
 	public static BufferedImage PLAYER = SpriteSheet.getImage(0, 536, 32, 64);
-	public static BufferedImage SHEEP_GENERIC = SpriteSheet.getImage(0, 524, 64, 32);
+	public static BufferedImage SHEEP_GENERIC = SpriteSheet.getImage(0, 504, 64, 32);
 	public static BufferedImage NPC_LUMBERJACK_A = SpriteSheet.getImage(0, 440, 32, 64);
+	
+	//Items
 	
 }
