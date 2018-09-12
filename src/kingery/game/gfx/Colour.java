ba743c363a89f0cc1c095b054080f1fd4ac3a95d
SeriousSirToad <lotsoflamps03@gmail.com>
2018-09-12 07:56:28 -0400

    Added colour class for later optimization

---
+++ src/kingery/game/gfx/Colour.java
@@ -0,0 +1,23 @@
+package kingery.game.gfx;
+
+import java.awt.image.BufferedImage;
+
+public class Colour {
+
+	public static BufferedImage fixYoSelf(int[] colors, int[] newColors,
+			BufferedImage image) {
+
+		for (int i = 0; i < colors.length; i++) {
+			for (int y = 0; y < image.getHeight(); y++) {
+				for (int x = 0; x < image.getWidth(); x++) {
+					if (image.getRGB(x, y) == colors[i]) {
+						image.setRGB(x, y, newColors[i]);
+					}
+				}
+			}
+		}
+		return image;
+
+	}
+
+}
